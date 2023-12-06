ad9ff4fe6af266d0a4d178b5fd42b2945d86f80b
Ghulam Suleman <sulemanbuzdar615@gmail.com>
2023-12-07 00:38:15 +0500

    torch Api check

--- JS/toggle.js
+++ JS/toggle.js
@@ -9,15 +9,17 @@ async function toggleFlashlight() {
             const isTorchOn = track.getSettings().torch === true;
             await track.applyConstraints({ advanced: [{ torch: !isTorchOn }] });
             console.log(`Flashlight turned ${isTorchOn ? 'off' : 'on'}.`);
+            document.getElementById('answer').innerText = 'Suppotted'
+
         } else {
 
-            document.getElementById('answer').innerHTML = 'Not Suppotted'
+            document.getElementById('answer').innerText = 'Not Suppotted'
             console.log('Torch mode not supported or cannot be controlled manually.');
         }
 
         track.stop();
     } catch (error) {
-        document.getElementById('answer').innerHTML = error
+        document.getElementById('answer').innerText = error
 
         console.error('Error accessing camera:', error);
     }
