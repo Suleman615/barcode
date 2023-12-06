27d9ab94fed03b8d2d6b40a5ad7e9b5edf82e75c
Ghulam Suleman <sulemanbuzdar615@gmail.com>
2023-12-06 17:27:23 +0500

    torch Api check

--- JS/toggle.js
+++ JS/toggle.js
@@ -1,22 +1,23 @@
 async function toggleFlashlight() {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
         const track = stream.getVideoTracks()[0];
 
         const capabilities = track.getCapabilities();
-        if (capabilities.torch && capabilities.torch.mode.includes('manual')) {
+
+        if (capabilities.torch && 'mode' in capabilities.torch && capabilities.torch.mode.includes('manual')) {
             const isTorchOn = track.getSettings().torch === true;
             await track.applyConstraints({ advanced: [{ torch: !isTorchOn }] });
             console.log(`Flashlight turned ${isTorchOn ? 'off' : 'on'}.`);
         } else {
-            document.getElementById('answer').innerText = "Not Supported"
-            console.log('Torch mode not supported.');
+            document.getElementById('answer').innerText = 'Not Supported'
+
+            console.log('Torch mode not supported or cannot be controlled manually.');
         }
 
         track.stop();
     } catch (error) {
         document.getElementById('answer').innerText = error
-
         console.error('Error accessing camera:', error);
     }
 }
