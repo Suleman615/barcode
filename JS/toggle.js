9e123e1ac42d4326b79173906d9a9ee6ec362029
Ghulam Suleman <sulemanbuzdar615@gmail.com>
2023-12-18 18:36:32 +0500

    flash checking

--- JS/toggle.js
+++ JS/toggle.js
@@ -3,55 +3,55 @@
 async function toggleFlashlight() {
     let torchaccess = 'torch' in navigator;
 
-    if (torchaccess) {
-        try {
-            // Try to access the back camera
-            const backStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-            const backTrack = backStream.getVideoTracks()[0];
+    // if (torchaccess) {
+    try {
+        // Try to access the back camera
+        const backStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+        const backTrack = backStream.getVideoTracks()[0];
 
-            // Check torch capabilities for the back camera
-            const backCapabilities = backTrack.getCapabilities();
+        // Check torch capabilities for the back camera
+        const backCapabilities = backTrack.getCapabilities();
 
-            if (backCapabilities.torch === true) {
-                const isBackTorchOn = backTrack.getSettings().torch === true;
-                await backTrack.applyConstraints({ advanced: [{ torch: !isBackTorchOn }] });
-                console.log(`Back flashlight turned ${isBackTorchOn ? 'off' : 'on'}.`);
-            } else {
+        if (backCapabilities.torch === true) {
+            const isBackTorchOn = backTrack.getSettings().torch === true;
+            await backTrack.applyConstraints({ advanced: [{ torch: !isBackTorchOn }] });
+            console.log(`Back flashlight turned ${isBackTorchOn ? 'off' : 'on'}.`);
+        } else {
 
-                document.getElementById('answer').innerText = 'Not Available'
-                console.log('Back torch mode not supported or cannot be controlled manually.');
-            }
-
-            backTrack.stop(); // Stop the back camera stream
-
-        } catch (backError) {
-            console.error('Error accessing back camera:', backError);
-
-            try {
-                // If accessing the back camera fails, try to access the front camera
-                const frontStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
-                const frontTrack = frontStream.getVideoTracks()[0];
-
-                // Check torch capabilities for the front camera
-                const frontCapabilities = frontTrack.getCapabilities();
-
-                if (frontCapabilities.torch === true) {
-                    const isFrontTorchOn = frontTrack.getSettings().torch === true;
-                    await frontTrack.applyConstraints({ advanced: [{ torch: !isFrontTorchOn }] });
-                    console.log(`Front flashlight turned ${isFrontTorchOn ? 'off' : 'on'}.`);
-                } else {
-                    console.log('Front torch mode not supported or cannot be controlled manually.');
-                }
-
-                frontTrack.stop(); // Stop the front camera stream
-
-            } catch (frontError) {
-                console.error('Error accessing front camera:', frontError);
-            }
+            document.getElementById('answer').innerText = 'Not Available'
+            console.log('Back torch mode not supported or cannot be controlled manually.');
         }
-    } else {
-        console.log("no torch")
+
+        backTrack.stop(); // Stop the back camera stream
+
+    } catch (backError) {
+        console.error('Error accessing back camera:', backError);
+
+        try {
+            // If accessing the back camera fails, try to access the front camera
+            const frontStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
+            const frontTrack = frontStream.getVideoTracks()[0];
+
+            // Check torch capabilities for the front camera
+            const frontCapabilities = frontTrack.getCapabilities();
+
+            if (frontCapabilities.torch === true) {
+                const isFrontTorchOn = frontTrack.getSettings().torch === true;
+                await frontTrack.applyConstraints({ advanced: [{ torch: !isFrontTorchOn }] });
+                console.log(`Front flashlight turned ${isFrontTorchOn ? 'off' : 'on'}.`);
+            } else {
+                console.log('Front torch mode not supported or cannot be controlled manually.');
+            }
+
+            frontTrack.stop(); // Stop the front camera stream
+
+        } catch (frontError) {
+            console.error('Error accessing front camera:', frontError);
+        }
     }
+    // } else {
+    //     console.log("no torch")
+    // }
 }
 
 
