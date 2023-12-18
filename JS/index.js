5f2f14b9dbed9300fea6f67b369b3d198719ec5c
Ghulam Suleman <sulemanbuzdar615@gmail.com>
2023-12-18 18:18:16 +0500

    analyser created

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,4 @@
 
-// // Check if the browser supports enumerateDevices
-// if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-//     // Enumerate all media devices
-//     navigator.mediaDevices.enumerateDevices()
-//         .then(function (devices) {
-//             // Check if there is a video input device (camera)
-//             const hasCamera = devices.some(device => device.kind === 'videoinput');
-
-//             if (hasCamera) {
-//                 console.log('Camera is available');
-//                 document.getElementById('scancode').style.display = 'block'
-//                 document.getElementById('inputmanually').style.display = 'none'
-//                 document.getElementById('result').style.display = 'none'
-
-
-//             } else {
-//                 document.getElementById('scancode').style.display = 'none'
-//                 document.getElementById('inputmanually').style.display = 'block'
-//                 document.getElementById('result').style.display = 'none'
-
-//             }
-//         })
-//         .catch(function (error) {
-//             console.error('Error enumerating devices:', error);
-//         });
-// } else {
-//     // enumerateDevices is not supported
-//     console.error('enumerateDevices is not supported');
-// }
 
 document.getElementById('scancode').style.display = 'none'
 document.getElementById('inputmanually').style.display = 'none'
@@ -59,6 +30,48 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 }
 
 
+
+
+
+
+
+// *******************Scanner functions**********************
+
+
+
+function onScanSuccess(decodedText, decodedResult) {
+
+    document.getElementById('result').innerHTML = `
+        <p>${decodedText}</p>
+        `
+    document.getElementById('scancode').style.display = 'none'
+
+    document.getElementById('result').style.display = 'block'
+
+
+
+    html5QrcodeScanner.clear();
+}
+
+
+
+function onScanFail(results) {
+    console.log(results)
+    document.getElementById('result').innerHTML = `<p>Nothing Found</p>`
+}
+
+let config = {
+    fps: 10,
+    qrbox: { width: 200, height: 200 },
+    rememberLastUsedCamera: false,
+    howTorchButtonIfSupported: true,
+    supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+};
+
+
+let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
+
+html5QrcodeScanner.render(onScanSuccess, onScanFail)
 
 
 
--- JS/toggle.js
+++ JS/toggle.js
@@ -1,50 +1,56 @@
 
 
 async function toggleFlashlight() {
-    try {
-        // Try to access the back camera
-        const backStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-        const backTrack = backStream.getVideoTracks()[0];
+    let torchaccess = 'torch' in navigator;
 
-        // Check torch capabilities for the back camera
-        const backCapabilities = backTrack.getCapabilities();
-
-        if (backCapabilities.torch === true) {
-            const isBackTorchOn = backTrack.getSettings().torch === true;
-            await backTrack.applyConstraints({ advanced: [{ torch: !isBackTorchOn }] });
-            console.log(`Back flashlight turned ${isBackTorchOn ? 'off' : 'on'}.`);
-        } else {
-
-            document.getElementById('answer').innerText = 'Not Available'
-            console.log('Back torch mode not supported or cannot be controlled manually.');
-        }
-
-        backTrack.stop(); // Stop the back camera stream
-
-    } catch (backError) {
-        console.error('Error accessing back camera:', backError);
-
+    if (torchaccess) {
         try {
-            // If accessing the back camera fails, try to access the front camera
-            const frontStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
-            const frontTrack = frontStream.getVideoTracks()[0];
+            // Try to access the back camera
+            const backStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+            const backTrack = backStream.getVideoTracks()[0];
 
-            // Check torch capabilities for the front camera
-            const frontCapabilities = frontTrack.getCapabilities();
+            // Check torch capabilities for the back camera
+            const backCapabilities = backTrack.getCapabilities();
 
-            if (frontCapabilities.torch === true) {
-                const isFrontTorchOn = frontTrack.getSettings().torch === true;
-                await frontTrack.applyConstraints({ advanced: [{ torch: !isFrontTorchOn }] });
-                console.log(`Front flashlight turned ${isFrontTorchOn ? 'off' : 'on'}.`);
+            if (backCapabilities.torch === true) {
+                const isBackTorchOn = backTrack.getSettings().torch === true;
+                await backTrack.applyConstraints({ advanced: [{ torch: !isBackTorchOn }] });
+                console.log(`Back flashlight turned ${isBackTorchOn ? 'off' : 'on'}.`);
             } else {
-                console.log('Front torch mode not supported or cannot be controlled manually.');
+
+                document.getElementById('answer').innerText = 'Not Available'
+                console.log('Back torch mode not supported or cannot be controlled manually.');
             }
 
-            frontTrack.stop(); // Stop the front camera stream
+            backTrack.stop(); // Stop the back camera stream
 
-        } catch (frontError) {
-            console.error('Error accessing front camera:', frontError);
+        } catch (backError) {
+            console.error('Error accessing back camera:', backError);
+
+            try {
+                // If accessing the back camera fails, try to access the front camera
+                const frontStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
+                const frontTrack = frontStream.getVideoTracks()[0];
+
+                // Check torch capabilities for the front camera
+                const frontCapabilities = frontTrack.getCapabilities();
+
+                if (frontCapabilities.torch === true) {
+                    const isFrontTorchOn = frontTrack.getSettings().torch === true;
+                    await frontTrack.applyConstraints({ advanced: [{ torch: !isFrontTorchOn }] });
+                    console.log(`Front flashlight turned ${isFrontTorchOn ? 'off' : 'on'}.`);
+                } else {
+                    console.log('Front torch mode not supported or cannot be controlled manually.');
+                }
+
+                frontTrack.stop(); // Stop the front camera stream
+
+            } catch (frontError) {
+                console.error('Error accessing front camera:', frontError);
+            }
         }
+    } else {
+        console.log("no torch")
     }
 }
 
